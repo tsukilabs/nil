bf77788a25bf91ec957cc15364ec02483832fb51
danilofs18 <32103557+danilofs18@users.noreply.github.com>
2025-02-01 16:43:40 -0300

    test: add offensive power test

--- crates/nil-core/src/battle.rs
+++ crates/nil-core/src/battle.rs
@@ -3,9 +3,9 @@ use bon::Builder;
 
 #[derive(Builder)]
 pub struct Battle {
-  #[builder(default)]
+  #[builder(default, into)]
   attacker: Vec<UnitBox>,
-  #[builder(default)]
+  #[builder(default, into)]
   defender: Vec<UnitBox>,
 }
 
@@ -17,15 +17,17 @@ impl Battle {
 
 #[derive(Copy, Clone, Debug)]
 pub struct OffensivePower {
-  pub value: f64,
+  pub total: f64,
   pub infantry_ratio: f64,
   pub cavalry_ratio: f64,
+  pub ranged_ratio: f64,
 }
 
 impl OffensivePower {
   pub fn new(units: &[UnitBox]) -> Self {
     let mut infantry = 0.0;
     let mut cavalry = 0.0;
+    let mut ranged = 0.0;
 
     for unit in units {
       match unit.kind() {
@@ -35,15 +37,19 @@ impl OffensivePower {
         UnitKind::Cavalry => {
           cavalry += unit.sum_attack();
         }
+        UnitKind::Ranged => {
+          ranged += unit.sum_attack();
+        }
       }
     }
 
-    let total = infantry + cavalry;
+    let total = infantry + cavalry + ranged;
 
     OffensivePower {
-      value: total,
+      total,
       infantry_ratio: infantry / total,
       cavalry_ratio: cavalry / total,
+      ranged_ratio: ranged / total,
     }
   }
 }
