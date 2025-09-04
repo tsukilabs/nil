d6e38f0b453d1059d062018404a794979b8e2fc7
Andrew Ferreira <andrew.shien2@gmail.com>
2025-09-04 14:26:34 -0300

    build: add license header (#99)

--- scripts/build-wasm.ps1
+++ scripts/build-wasm.ps1
@@ -3,7 +3,10 @@
 
 param(
   [Alias('R')]
-  [switch]$Release
+  [switch]$Release,
+
+  [Alias('P')]
+  [switch]$Publish
 )
 
 $ErrorActionPreference = 'Stop'
@@ -12,14 +15,37 @@ $PSNativeCommandUseErrorActionPreference = $true
 function Build {
   param ([string] $Crate)
   
-  if (Test-Path -Path "crates/$Crate/pkg" ) {
-    Get-ChildItem "crates/$Crate/pkg" -Recurse | Remove-Item
+  $Path = "crates/$Crate/pkg"
+
+  if (Test-Path -Path $Path ) {
+    Get-ChildItem -Path $Path -Recurse | Remove-Item
   }
 
   $BuildCmd = "wasm-pack build crates/$Crate --scope tsukilabs"
-  $BuildCmd += if ($Release) { ' --release' }  else { ' --dev --no-opt' }
+  $BuildCmd += if ($Release -or $Publish) {
+    ' --release'
+  }
+  else {
+    ' --dev --no-opt' 
+  }
 
   Invoke-Expression $BuildCmd
+
+  if ($Publish) {
+    $Extension = $('*.js', '*.ts')
+    $Files = Get-ChildItem -Path $Path -Recurse -Name -Include $Extension 
+    | ForEach-Object { Join-Path -Path $Path -ChildPath $_ }
+
+    foreach ($File in $Files) {
+      $Contents = "// Copyright (C) Call of Nil contributors`n"
+      $Contents += "// SPDX-License-Identifier: AGPL-3.0-only`n"
+      $Contents += "`n$(Get-Content -Path $File -Raw)"
+
+      Set-Content -Path $File -Value $Contents
+    }
+
+    Invoke-Expression "pnpm -F @tsukilabs/$Crate publish --access public"
+  }
 }
 
 Build -Crate 'nil-continent'
