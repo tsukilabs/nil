46c1c43225957d51d888379c02b1a334eebe00ac
Andrew Ferreira <andrew.shien2@gmail.com>
2025-12-02 17:48:42 -0300

    docs: deploy website

---
+++ docs/.vitepress/data/latest.data.ts
@@ -0,0 +1,21 @@
+export default {
+  async load() {
+    const response = await fetch(
+      'https://github.com/tsukilabs/nil/releases/latest/download/latest.json',
+    );
+
+    return response.json();
+  },
+};
+
+export interface Latest {
+  readonly version: string;
+  readonly pub_date: string;
+  readonly platforms: {
+    readonly 'linux-x86_64-appimage': { readonly url: string; };
+    readonly 'linux-x86_64-deb': { readonly url: string; };
+    readonly 'linux-x86_64-rpm': { readonly url: string; };
+    readonly 'windows-x86_64-nsis': { readonly url: string; };
+    readonly 'windows-x86_64-msi': { readonly url: string; };
+  };
+}
