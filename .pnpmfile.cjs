d49576e6fc20c81a18059c85cf3508739446c907
Andrew Ferreira <andrew.shien2@gmail.com>
2026-01-29 12:15:32 -0300

    build: use latest version

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -3,9 +3,8 @@ const { env } = require('node:process');
 function readPackage(pkg) {
   if (env.NIL_MAP_WORKSPACE_DEPS === 'true' && pkg.dependencies) {
     const dependencies = {};
-    const version = `^${require('./package.json').version}`;
     for (const [key, value] of Object.entries(pkg.dependencies)) {
-      dependencies[key] = key.startsWith('@tsukilabs/nil') ? version : value;
+      dependencies[key] = key.startsWith('@tsukilabs/nil') ? 'latest' : value;
     }
 
     pkg.dependencies = dependencies;
