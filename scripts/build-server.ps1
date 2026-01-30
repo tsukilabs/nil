509c62f80020a18bc2d7bac606253cb8683e4e85
Andrew Ferreira <andrew.shien2@gmail.com>
2026-01-30 03:05:04 -0300

    build: add script

---
+++ scripts/build-server.ps1
@@ -0,0 +1,17 @@
+# Copyright (C) Call of Nil contributors
+# SPDX-License-Identifier: AGPL-3.0-only
+
+param(
+  [Alias('T')]
+  [string]$TargetDir
+)
+
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+cargo build --profile release-server --package nil-server
+
+if ($TargetDir) {
+  $File = if ($IsWindows) { 'nil-server.exe' } else { 'nil-server' }
+  Copy-Item "./target/release-server/$File" -Destination $TargetDir
+}
