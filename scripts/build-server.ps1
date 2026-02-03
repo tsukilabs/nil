cbd2d43d12bb6623dcf258bfdc9c3dedbf79d507
Andrew Ferreira <andrew.shien2@gmail.com>
2026-02-03 12:13:57 -0300

    build: fix android build (#258)

--- scripts/build-server.ps1
+++ scripts/build-server.ps1
@@ -22,19 +22,16 @@ if ($TargetDir) {
   Copy-Item $Path -Destination (Resolve-Path $TargetDir).ToString()
 }
 
-if ($Release -and ($IsWindows -or $IsLinux)) {
+if ($Release -and $IsLinux) {
   $Version = Get-Content -Path 'package.json' -Raw
   | ConvertFrom-Json
   | Select-Object -ExpandProperty 'version'
 
-  $Name = "Call.of.Nil_$($Version)_server"
-  if ($IsWindows) {
-    $Name += '.exe'
-  }
+  $AssetName = "Call.of.Nil_$($Version)_server"
 
-  Rename-Item -Path $Path -NewName $Name
-  
-  $Path = (Resolve-Path "./target/release-server/$Name").ToString()
+  Rename-Item -Path $Path -NewName $AssetName
+
+  $Path = (Resolve-Path "./target/release-server/$AssetName").ToString()
 
   $TagName = gh release view --json tagName -R 'tsukilabs/nil'
   | ConvertFrom-Json 
@@ -42,28 +39,33 @@ if ($Release -and ($IsWindows -or $IsLinux)) {
 
   gh release upload --clobber $TagName $Path -R 'tsukilabs/nil'
 
-  if ($IsLinux) {
-    $Token = [Environment]::GetEnvironmentVariable('TSUKILABS_TOKEN')
-    $WebhookUrl = [Environment]::GetEnvironmentVariable('NIL_DISCORD_WEBHOOK_URL')
+  $Token = [Environment]::GetEnvironmentVariable('TSUKILABS_TOKEN')
+  $WebhookUrl = [Environment]::GetEnvironmentVariable('NIL_DISCORD_WEBHOOK_URL')
 
-    if ($Token) {
-      $Uri = 'http://tsukilabs.dev.br/release/nil'
-      $Headers = @{
-        Authorization = "Bearer $Token"
+  if ($Token) {
+    $Uri = 'http://tsukilabs.dev.br/release/nil'
+    $Headers = @{
+      Authorization = "Bearer $Token"
+    }
+
+    Invoke-WebRequest -Uri $Uri -Headers $Headers | Out-Null
+
+    if ($WebhookUrl) {
+      $AssetUrl = gh release view $TagName --json assets -R 'tsukilabs/nil'
+      | ConvertFrom-Json 
+      | Select-Object -ExpandProperty 'assets'
+      | Where-Object { $_.name -eq $AssetName }
+      | Select-Object -ExpandProperty 'url'
+      
+      $Message = "Server updated to v$Version"
+      $Message += "`n<https://github.com/tsukilabs/nil/releases/tag/$TagName>"
+      $Message += "`n<$AssetUrl>"
+
+      $Body = @{
+        content = $Message
       }
 
-      Invoke-WebRequest -Uri $Uri -Headers $Headers
-
-      if ($WebhookUrl) {
-        $Message = "Server updated to v$Version."
-        $Message += "`nhttps://github.com/tsukilabs/nil/releases/tag/$TagName"
-
-        $Body = @{
-          content = $Message
-        }
-
-        Invoke-WebRequest -Uri $WebhookUrl -Method Post -Body $Body
-      }
+      Invoke-WebRequest -Uri $WebhookUrl -Method Post -Body $Body | Out-Null
     }
   }
 }
