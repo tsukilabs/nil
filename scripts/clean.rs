de681c62df902f176496d7cdfd4223c0876cdab0
Andrew Ferreira <andrew.shien2@gmail.com>
2026-02-15 20:27:24 -0300

    chore: also clean `node_modules`

--- scripts/clean.rs
+++ scripts/clean.rs
@@ -6,20 +6,49 @@ edition = "2024"
 [dependencies]
 anyhow = "1.0"
 
+[dependencies.clap]
+version = "4.5"
+features = ["derive"]
+
 [dependencies.nil-util]
 path = "../crates/nil-util"
 ---
 
-
 use anyhow::Result;
+use clap::Parser;
 use nil_util::spawn;
 use std::fs;
+use std::path::Path;
+
+#[derive(Parser)]
+struct Args {
+  #[arg(long)]
+  node: bool,
+}
 
 fn main() -> Result<()> {
-  spawn!("cargo clean --verbose")?;
+  let args = Args::parse();
 
-  if fs::exists("app/src-tauri/target")? {
-    fs::remove_dir_all("app/src-tauri/target")?;
+  spawn!("cargo clean --verbose")?;
+  remove_dir("app/src-tauri/target")?;
+
+  remove_dir("app/dist")?;
+  remove_dir("docs/dist")?;
+
+  if args.node {
+    remove_dir("node_modules")?;
+    remove_dir("app/node_modules")?;
+    remove_dir("docs/node_modules")?;
+  }
+
+  Ok(())
+}
+
+fn remove_dir(path: &str) -> Result<()> {
+  if fs::exists(path)? {
+    let path = Path::new(path).canonicalize()?;
+    println!("Removing {}", path.to_string_lossy());
+    fs::remove_dir_all(path)?;
   }
 
   Ok(())
