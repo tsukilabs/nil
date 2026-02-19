838bf6fb90689b3cb2bc13e61b22035deeb9027b
Andrew Ferreira <andrew.shien2@gmail.com>
2026-02-19 09:44:54 -0300

    heterogeneous try blocks

--- crates/nil-server/src/lib.rs
+++ crates/nil-server/src/lib.rs
@@ -2,7 +2,12 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 #![doc(html_favicon_url = "https://nil.dev.br/favicon.png")]
-#![feature(iterator_try_collect, try_blocks, try_trait_v2)]
+#![feature(
+  iterator_try_collect,
+  try_blocks,
+  try_blocks_heterogeneous,
+  try_trait_v2
+)]
 
 mod app;
 mod error;
