3c5f7c7a5b2d66fc1449a41f85ffcf774b3010b2
Andrew Ferreira <andrew.shien2@gmail.com>
2026-03-17 12:19:06 -0300

    fix: set cors header

---
+++ crates/nil-server/src/router/auth.rs
@@ -0,0 +1,73 @@
+// Copyright (C) Call of Nil contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use crate::app::App;
+use crate::error::Error;
+use crate::middleware::authorization::{decode_jwt, encode_jwt};
+use crate::res;
+use crate::response::from_database_err;
+use axum::extract::{Json, State};
+use axum::response::Response;
+use nil_core::player::PlayerId;
+use nil_payload::{AuthorizeRequest, ValidateTokenRequest};
+use nil_server_types::ServerKind;
+use tokio::task::spawn_blocking;
+
+pub async fn authorize(State(app): State<App>, Json(req): Json<AuthorizeRequest>) -> Response {
+  let Ok(result) = spawn_blocking(move || {
+    match app.server_kind() {
+      ServerKind::Local { .. } => encode_jwt(req.player),
+      ServerKind::Remote => {
+        let Some(password) = req.password else {
+          return Err(Error::MissingPassword);
+        };
+
+        if app
+          .database()
+          .get_user(&req.player)
+          .map_err(Into::<Error>::into)?
+          .verify_password(&password)
+        {
+          encode_jwt(req.player)
+        } else {
+          Err(Error::IncorrectUserCredentials)
+        }
+      }
+    }
+  })
+  .await
+  else {
+    return res!(INTERNAL_SERVER_ERROR);
+  };
+
+  result
+    .map(|token| res!(OK, Json(token)))
+    .unwrap_or_else(Response::from)
+}
+
+pub async fn validate_token(
+  State(app): State<App>,
+  Json(req): Json<ValidateTokenRequest>,
+) -> Response {
+  let Ok(player) = spawn_blocking(move || {
+    decode_jwt(&req.token)
+      .map(|token| token.claims.sub)
+      .ok()
+  })
+  .await
+  else {
+    return res!(INTERNAL_SERVER_ERROR);
+  };
+
+  if app.server_kind().is_remote()
+    && let Some(player) = player.clone()
+  {
+    match app.database().user_exists(player) {
+      Ok(true) => {}
+      Ok(false) => return res!(OK, Json(None::<PlayerId>)),
+      Err(err) => return from_database_err(err),
+    }
+  }
+
+  res!(OK, Json(player))
+}
---
+++ crates/nil-server/src/router/websocket.rs
@@ -0,0 +1,38 @@
+// Copyright (C) Call of Nil contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use crate::app::App;
+use crate::error::Error;
+use crate::middleware::authorization::CurrentPlayer;
+use crate::response::from_database_err;
+use crate::websocket::handle_socket;
+use axum::extract::ws::WebSocketUpgrade;
+use axum::extract::{Extension, Query, State};
+use axum::response::Response;
+use nil_core::world::World;
+use nil_payload::WebsocketQuery;
+
+pub async fn websocket(
+  ws: WebSocketUpgrade,
+  State(app): State<App>,
+  Extension(player): Extension<CurrentPlayer>,
+  Query(query): Query<WebsocketQuery>,
+) -> Response {
+  if app.server_kind().is_remote() {
+    match app
+      .database()
+      .verify_game_password(query.world_id, query.world_password.as_ref())
+    {
+      Ok(true) => {}
+      Ok(false) => return Error::IncorrectWorldCredentials(query.world_id).into(),
+      Err(err) => return from_database_err(err),
+    }
+  }
+
+  let id = player.0;
+  app
+    .world(query.world_id, World::subscribe)
+    .await
+    .map_left(|listener| ws.on_upgrade(move |socket| handle_socket(socket, listener, id)))
+    .into_inner()
+}
