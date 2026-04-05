a4668224d65b60c46ab1c04d208a9d61a7c1a3b7
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-05 11:28:41 -0300

    fix: set overflow behavior

--- crates/nil-lua/examples/get-remote-worlds.lua
+++ crates/nil-lua/examples/get-remote-worlds.lua
@@ -6,9 +6,11 @@ local worlds = client:getRemoteWorlds();
 for _, world in ipairs(worlds) do
   println(world.config.name)
   println("  id: ", world.config.id)
+  println("  created by: ", world.createdBy)
   println("  round: ", world.currentRound)
   println("  size: ", world.continentSize)
   println("  active players: ", world.activePlayers)
   println("  total players: ", world.totalPlayers)
-  println("  created by: ", world.createdBy)
+  println("  world speed: ", world.config.speed)
+  println("  unit speed: ", world.config.unitSpeed)
 end
