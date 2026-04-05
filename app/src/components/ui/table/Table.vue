37195db3ae82aaa10cd1d0bc992615d563de35a3
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-05 02:07:54 -0300

    fix: fix table component

--- app/src/components/ui/table/Table.vue
+++ app/src/components/ui/table/Table.vue
@@ -1,16 +1,45 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue';
 import { cn } from '@/components/ui/utils';
+import { type Option, toPixel } from '@tb-dev/utils';
+import { computed, type CSSProperties, type HTMLAttributes, type VNode } from 'vue';
 
 const props = defineProps<{
   class?: HTMLAttributes['class'];
+  height?: number | string;
+  style?: HTMLAttributes['style'];
+  tableClass?: HTMLAttributes['class'];
+  tableStyle?: HTMLAttributes['style'];
+  width?: number | string;
 }>();
+
+defineSlots<{ default: () => VNode; }>();
+
+const containerHeight = computed<Option<CSSProperties>>(() => {
+  return props.height ? { maxHeight: toPixel(props.height) } : null;
+});
+
+const containerWidth = computed<Option<CSSProperties>>(() => {
+  return props.width ? { maxWidth: toPixel(props.width) } : null;
+});
 </script>
 
 <template>
-  <div data-slot="table-container" class="relative w-full overflow-auto">
-    <table data-slot="table" :class="cn('w-full caption-bottom text-sm', props.class)">
-      <slot />
+  <div
+    data-slot="table-container"
+    :style="[containerHeight, containerWidth, props.style]"
+    :class="cn(
+      'relative w-full',
+      containerWidth ? 'overflow-x-auto' : 'overflow-x-hidden',
+      containerHeight ? 'overflow-y-auto' : 'overflow-y-hidden',
+      props.class,
+    )"
+  >
+    <table
+      data-slot="table"
+      :style="props.tableStyle"
+      :class="cn('w-full caption-bottom text-sm', props.tableClass)"
+    >
+      <slot></slot>
     </table>
   </div>
 </template>
