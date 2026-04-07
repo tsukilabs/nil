4fe31c05002cc78280e4d28684dba2decc710a02
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-06 23:32:43 -0300

    rename arg

--- crates/nil-server-database/src/database/mod.rs
+++ crates/nil-server-database/src/database/mod.rs
@@ -45,40 +45,40 @@ impl Database {
 
   pub async fn count_games(&self) -> Result<i64> {
     self
-      .with_blocking(|inner| inner.count_games())
+      .with_blocking(|db| db.count_games())
       .await
   }
 
   pub async fn count_games_by_user(&self, id: impl Into<PlayerId>) -> Result<i64> {
     let id: PlayerId = id.into();
     self
-      .with_blocking(move |inner| inner.count_games_by_user(id))
+      .with_blocking(move |db| db.count_games_by_user(id))
       .await
   }
 
   pub async fn count_games_by_user_id(&self, id: impl Into<UserId>) -> Result<i64> {
     let id: UserId = id.into();
     self
-      .with_blocking(move |inner| inner.count_games_by_user_id(id))
+      .with_blocking(move |db| db.count_games_by_user_id(id))
       .await
   }
 
   pub async fn create_game(&self, new: NewGame) -> Result<usize> {
     self
-      .with_blocking(move |inner| inner.create_game(&new))
+      .with_blocking(move |db| db.create_game(&new))
       .await
   }
 
   pub async fn create_user(&self, new: NewUser) -> Result<usize> {
     self
-      .with_blocking(move |inner| inner.create_user(&new))
+      .with_blocking(move |db| db.create_user(&new))
       .await
   }
 
   pub async fn delete_game(&self, id: impl Into<GameId>) -> Result<usize> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.delete_game(id))
+      .with_blocking(move |db| db.delete_game(id))
       .await
   }
 
@@ -88,109 +88,107 @@ impl Database {
   {
     let games = games.into_iter().collect_vec();
     self
-      .with_blocking(move |inner| inner.delete_games(&games))
+      .with_blocking(move |db| db.delete_games(&games))
       .await
   }
 
   pub async fn game_exists(&self, id: impl Into<GameId>) -> Result<bool> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.game_exists(id))
+      .with_blocking(move |db| db.game_exists(id))
       .await
   }
 
   pub async fn get_game(&self, id: impl Into<GameId>) -> Result<Game> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.get_game(id))
+      .with_blocking(move |db| db.get_game(id))
       .await
   }
 
   pub async fn get_game_creator(&self, id: impl Into<GameId>) -> Result<PlayerId> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.get_game_creator(id))
+      .with_blocking(move |db| db.get_game_creator(id))
       .await
   }
 
   pub async fn get_game_ids(&self) -> Result<Vec<GameId>> {
     self
-      .with_blocking(|inner| inner.get_game_ids())
+      .with_blocking(|db| db.get_game_ids())
       .await
   }
 
   pub async fn get_game_password(&self, id: impl Into<GameId>) -> Result<Option<HashedPassword>> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.get_game_password(id))
+      .with_blocking(move |db| db.get_game_password(id))
       .await
   }
 
   pub async fn get_game_round_duration(&self, id: impl Into<GameId>) -> Result<Option<Duration>> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.get_game_round_duration(id))
+      .with_blocking(move |db| db.get_game_round_duration(id))
       .await
   }
 
   pub async fn get_game_with_blob(&self, id: impl Into<GameId>) -> Result<GameWithBlob> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.get_game_with_blob(id))
+      .with_blocking(move |db| db.get_game_with_blob(id))
       .await
   }
 
   pub async fn get_games(&self) -> Result<Vec<Game>> {
-    self
-      .with_blocking(|inner| inner.get_games())
-      .await
+    self.with_blocking(|db| db.get_games()).await
   }
 
   pub async fn get_games_with_blob(&self) -> Result<Vec<GameWithBlob>> {
     self
-      .with_blocking(|inner| inner.get_games_with_blob())
+      .with_blocking(|db| db.get_games_with_blob())
       .await
   }
 
   pub async fn get_user(&self, id: impl Into<PlayerId>) -> Result<User> {
     let id: PlayerId = id.into();
     self
-      .with_blocking(move |inner| inner.get_user(id))
+      .with_blocking(move |db| db.get_user(id))
       .await
   }
 
   pub async fn get_user_by_id(&self, id: impl Into<UserId>) -> Result<User> {
     let id: UserId = id.into();
     self
-      .with_blocking(move |inner| inner.get_user_by_id(id))
+      .with_blocking(move |db| db.get_user_by_id(id))
       .await
   }
 
   pub async fn get_user_id(&self, id: impl Into<PlayerId>) -> Result<UserId> {
     let id: PlayerId = id.into();
     self
-      .with_blocking(move |inner| inner.get_user_id(id))
+      .with_blocking(move |db| db.get_user_id(id))
       .await
   }
 
   pub async fn get_user_player_id(&self, id: impl Into<UserId>) -> Result<PlayerId> {
     let id: UserId = id.into();
     self
-      .with_blocking(move |inner| inner.get_user_player_id(id))
+      .with_blocking(move |db| db.get_user_player_id(id))
       .await
   }
 
   pub async fn update_game_blob(&self, id: impl Into<GameId>, blob: Vec<u8>) -> Result<usize> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.update_game_blob(id, &blob))
+      .with_blocking(move |db| db.update_game_blob(id, &blob))
       .await
   }
 
   pub async fn user_exists(&self, id: impl Into<PlayerId>) -> Result<bool> {
     let id: PlayerId = id.into();
     self
-      .with_blocking(move |inner| inner.user_exists(&id))
+      .with_blocking(move |db| db.user_exists(&id))
       .await
   }
 
@@ -201,7 +199,7 @@ impl Database {
   ) -> Result<bool> {
     let id: GameId = id.into();
     self
-      .with_blocking(move |inner| inner.verify_game_password(id, password.as_ref()))
+      .with_blocking(move |db| db.verify_game_password(id, password.as_ref()))
       .await
   }
 
@@ -213,7 +211,7 @@ impl Database {
     let game_id: GameId = game_id.into();
     let player_id: PlayerId = player_id.into();
     self
-      .with_blocking(move |inner| inner.was_game_created_by(game_id, &player_id))
+      .with_blocking(move |db| db.was_game_created_by(game_id, &player_id))
       .await
   }
 }
