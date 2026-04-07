3135373a128aa39b070d51bd1fcb6785a4e0df9f
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-06 23:42:00 -0300

    docs: update `docs.rs` config

--- crates/nil-server/src/lib.rs
+++ crates/nil-server/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright (C) Call of Nil contributors
 // SPDX-License-Identifier: AGPL-3.0-only
 
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc(html_favicon_url = "https://nil.dev.br/favicon.png")]
 #![feature(
   duration_constructors,
