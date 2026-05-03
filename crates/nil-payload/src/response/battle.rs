d8eb00540797b444c20d7e76d86a9fcf4098b01d
Andrew Ferreira <andrew.shien2@gmail.com>
2026-05-03 16:47:30 -0300

    imports

--- crates/nil-payload/src/response/battle.rs
+++ crates/nil-payload/src/response/battle.rs
@@ -4,6 +4,9 @@
 use nil_core::battle::BattleResult;
 use serde::{Deserialize, Serialize};
 
+#[cfg(feature = "axum")]
+use nil_payload_macros::IntoJsonResponse;
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
-#[cfg_attr(feature = "axum", derive(nil_payload_macros::IntoJsonResponse))]
+#[cfg_attr(feature = "axum", derive(IntoJsonResponse))]
 pub struct SimulateBattleResponse(pub BattleResult);
--- crates/nil-payload/src/response/cheat/behavior.rs
+++ crates/nil-payload/src/response/cheat/behavior.rs
@@ -1,2 +1,7 @@
 // Copyright (C) Call of Nil contributors
 // SPDX-License-Identifier: AGPL-3.0-only
+
+use serde::{Deserialize, Serialize};
+
+#[cfg(feature = "axum")]
+use nil_payload_macros::IntoJsonResponse;
