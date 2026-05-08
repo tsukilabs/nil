413269925bd7ff444a9f7d0873a2cf5684ef5941
Andrew Ferreira <andrew.shien2@gmail.com>
2026-05-08 13:56:52 -0300

    fix: set database constraints

--- crates/nil-server-database/migrations/2026-01-26-232422-0000_create_user/up.sql
+++ crates/nil-server-database/migrations/2026-01-26-232422-0000_create_user/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user (
   id INTEGER NOT NULL PRIMARY KEY,
-  player_id TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL,
-  created_at TEXT NOT NULL,
-  updated_at TEXT NOT NULL
+  player_id TEXT NOT NULL UNIQUE CHECK (length(player_id) > 0),
+  password TEXT NOT NULL CHECK (length(password) > 0),
+  created_at TEXT NOT NULL CHECK (length(created_at) > 0),
+  updated_at TEXT NOT NULL CHECK (length(updated_at) > 0)
 )
