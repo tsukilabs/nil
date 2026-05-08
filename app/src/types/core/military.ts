cc03ce89e829ae4d7f03264c461953edf25ef32b
Andrew Ferreira <andrew.shien2@gmail.com>
2026-05-08 00:06:31 -0300

    remove military

--- app/src/types/core/military.ts
+++ app/src/types/core/military.ts
@@ -1,21 +1,7 @@
 // Copyright (C) Call of Nil contributors
 // SPDX-License-Identifier: AGPL-3.0-only
 
-import type {
-  Army,
-  ArmyPersonnel,
-  ContinentIndex,
-  ContinentSize,
-  Maneuver,
-  ManeuverId,
-  Squad,
-} from '@/types/bindings';
-
-export interface Military {
-  readonly continent: ReadonlyMap<ContinentIndex, readonly Army[]>;
-  readonly continentSize: ContinentSize;
-  readonly maneuvers: ReadonlyMap<ManeuverId, Maneuver>;
-}
+import type { ArmyPersonnel, Squad } from '@/types/bindings';
 
 export interface ArmyAcademyPersonnel {
   readonly archer: Squad;
