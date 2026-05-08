e81f4f51d61d3def999ce9b56672312c8c680ee4
Andrew Ferreira <andrew.shien2@gmail.com>
2026-05-08 13:59:17 -0300

    fix: set database constraints (#431)

--- crates/nil-server-database/migrations/2026-01-26-232513-0000_create_game/up.sql
+++ crates/nil-server-database/migrations/2026-01-26-232513-0000_create_game/up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE game (
-  id TEXT NOT NULL PRIMARY KEY,
-  password TEXT,
+  id TEXT NOT NULL PRIMARY KEY CHECK (length(id) > 0),
+  password TEXT CHECK (password IS NULL OR length(password) > 0),
   description TEXT,
-  round_duration TEXT,
-  server_version TEXT NOT NULL,
+  round_duration TEXT CHECK (round_duration IS NULL OR length(round_duration) > 0),
+  server_version TEXT NOT NULL CHECK (length(server_version) > 0),
   created_by INTEGER NOT NULL,
-  created_at TEXT NOT NULL,
-  updated_at TEXT NOT NULL,
+  created_at TEXT NOT NULL CHECK (length(created_at) > 0),
+  updated_at TEXT NOT NULL CHECK (length(updated_at) > 0),
   world_blob BLOB NOT NULL,
   FOREIGN KEY (created_by) REFERENCES user (id) ON DELETE CASCADE ON UPDATE CASCADE
 )
