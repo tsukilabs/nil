3147bb6edd092abe9ec11ca4c5bb31211296cb89
Andrew Ferreira <andrew.shien2@gmail.com>
2026-05-12 11:58:49 -0300

    minimumReleaseAge

--- app/src/components/ui/select/SelectContent.vue
+++ app/src/components/ui/select/SelectContent.vue
@@ -40,7 +40,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
         :class="cn(
           'p-1',
           position === 'popper' &&
-            'h-[var(--reka-select-trigger-height)] w-full min-w-[var(--reka-select-trigger-width)] scroll-my-1',
+            'h-(--reka-select-trigger-height) w-full min-w-(--reka-select-trigger-width) scroll-my-1',
         )"
       >
         <slot />
