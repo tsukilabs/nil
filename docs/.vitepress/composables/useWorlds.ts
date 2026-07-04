786c8903da93b22fac8cd302d0c5274293ec8e77
Andrew Ferreira <andrew.shien2@gmail.com>
2026-07-04 13:04:47 -0300

    docs: fix download urls (#469)

--- docs/.vitepress/composables/useWorlds.ts
+++ docs/.vitepress/composables/useWorlds.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
 import { get } from "../lib/api";
+import { compare } from "../lib/intl";
 import { inBrowser } from "vitepress";
 import { asyncRef } from "@tb-dev/vue";
 import type { RemoteWorld } from "@tsukilabs/nil-bindings";
@@ -11,7 +12,7 @@ export function useWorlds() {
     if (inBrowser) {
       const response = await get("get-remote-worlds");
       const worlds: RemoteWorld[] = await response.json();
-      return worlds;
+      return worlds.toSorted((a, b) => compare(a.config.name, b.config.name));
     }
     else {
       return [];
