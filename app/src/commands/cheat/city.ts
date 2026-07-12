91d198672857f4066296684d87cc23b3996609bc
Andrew Ferreira <andrew.shien2@gmail.com>
2026-07-12 11:09:30 -0300

    spawn

--- app/src/commands/cheat/city.ts
+++ app/src/commands/cheat/city.ts
@@ -6,8 +6,11 @@ import { invoke } from "@tauri-apps/api/core";
 import { CoordImpl } from "@/core/model/continent/coord";
 import type { ContinentKey } from "@/types/core/continent";
 import type {
+  BotId,
   CheatSetStabilityRequest,
   CheatSpawnCityRequest,
+  PlayerId,
+  PrecursorId,
   Ruler,
 } from "@tsukilabs/nil-bindings";
 
@@ -33,4 +36,16 @@ export async function cheatSpawnCity(ruler: Ruler, coord: ContinentKey) {
   };
 
   await invoke("cheat_spawn_city", { req });
+}
+
+export async function cheatSpawnBotCity(id: BotId, coord: ContinentKey) {
+  return cheatSpawnCity({ kind: "bot", id }, coord);
+}
+
+export async function cheatSpawnPlayerCity(id: PlayerId, coord: ContinentKey) {
+  return cheatSpawnCity({ kind: "player", id }, coord);
+}
+
+export async function cheatSpawnPrecursorCity(id: PrecursorId, coord: ContinentKey) {
+  return cheatSpawnCity({ kind: "precursor", id }, coord);
 }
