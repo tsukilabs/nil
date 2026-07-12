22e2af29eb29e85dddab78cd017d63246dc9c697
Andrew Ferreira <andrew.shien2@gmail.com>
2026-07-12 14:29:24 -0300

    edit_release_notes

--- scripts/build-server.rs
+++ scripts/build-server.rs
@@ -28,8 +28,10 @@ features = ["full"]
 use anyhow::{Context, Result};
 use clap::Parser;
 use nil_util::{spawn, spawn_fmt};
+use octocrab::Octocrab;
 use serde::Deserialize;
 use serde_json::from_slice;
+use std::env::var;
 use std::path::PathBuf;
 use std::{env, fs};
 
@@ -64,7 +66,10 @@ async fn main() -> Result<()> {
 
     fs::rename(path, &asset_path)?;
 
-    let octocrab = octocrab::instance();
+    let octocrab = Octocrab::builder()
+      .personal_token(var("GH_TOKEN")?)
+      .build()?;
+
     let repository = octocrab.repos("tsukilabs", "nil");
     let tag_name = repository
       .releases()
@@ -73,6 +78,15 @@ async fn main() -> Result<()> {
       .tag_name;
 
     upload_asset(&tag_name, &asset_path)?;
+
+    let notes = repository
+      .releases()
+      .generate_release_notes(&tag_name)
+      .send()
+      .await?
+      .body;
+
+    edit_release_notes(&tag_name, &notes)?;
 
     if let Ok(token) = env::var("TSUKILABS_TOKEN") {
       ureq::get("https://tsukilabs.dev.br/release/nil")
@@ -88,6 +102,15 @@ async fn main() -> Result<()> {
 fn upload_asset(tag_name: &str, path: &str) -> Result<()> {
   spawn_fmt!("gh release upload --clobber {tag_name} {path} -R tsukilabs/nil")
     .context("failed to upload asset")
+}
+
+fn edit_release_notes(tag_name: &str, notes: &str) -> Result<()> {
+  let notes = format!(
+    "This is an early preview. The game is still under development and not yet ready to play.\n\n{notes}"
+  );
+
+  spawn_fmt!("gh release edit {tag_name} --notes {notes} --verify-tag -R tsukilabs/nil")
+    .context("failed to edit release notes")
 }
 
 #[derive(Deserialize)]
